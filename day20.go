457941b99fa7f253c01ecfbc019b146ee6e43c8c
Alexey Vasilyev <alexey@alexadotlife.com>
2021-12-20 20:47:23 +0100

    minor improvements day20

--- day20.go
+++ day20.go
@@ -17,26 +17,18 @@ func square3x3() []pos {
 	return []pos{pos{-1, -1}, pos{0, -1}, pos{1, -1}, pos{-1, 0}, pos{0, 0}, pos{1, 0}, pos{-1, 1}, pos{0, 1}, pos{1, 1}}
 }
 
-func squareToInt(m posMap, p pos, inv bool) int {
+func squareToInt(m posMap, p pos, defaultValue int) int {
 	bin := make([]rune, 0, 9)
 	for _, n := range square3x3() {
 		pp := pos{p.x + n.x, p.y + n.y}
 		v, ok := m[pp]
 		if !ok {
-			v = 0
+			v = defaultValue
 		}
-		if !inv {
-			if v == 0 {
-				bin = append(bin, '0')
-			} else if v == 1 {
-				bin = append(bin, '1')
-			}
-		} else {
-			if v == 0 {
-				bin = append(bin, '1')
-			} else if v == 1 {
-				bin = append(bin, '0')
-			}
+		if v == 0 {
+			bin = append(bin, '0')
+		} else if v == 1 {
+			bin = append(bin, '1')
 		}
 	}
 	b := new(big.Int)
@@ -44,89 +36,21 @@ func squareToInt(m posMap, p pos, inv bool) int {
 	return int(b.Int64())
 }
 
-func step(m posMap, h map[int]int, inv bool) posMap {
+func step(m posMap, h map[int]int, defaultValue int) posMap {
 	nm := posMap{}
-	for p, v := range m {
-		if v != 1 {
-			continue
-		}
+	for p, _ := range m {
 		for _, n := range square3x3() {
 			pp := pos{p.x + n.x, p.y + n.y}
 			_, ok := nm[pp]
 			if ok {
 				continue
 			}
-			intVal := squareToInt(m, pp, inv)
+			intVal := squareToInt(m, pp, defaultValue)
 			vv, _ := h[intVal]
 			nm[pp] = vv
 		}
 	}
 	return nm
-}
-
-func invertMap(m posMap) posMap {
-	n := posMap{}
-	for p, v := range m {
-		if v == 1 {
-			n[p] = 0
-		} else if v == 0 {
-			n[p] = 1
-		}
-	}
-	return n
-}
-
-func invertH(h map[int]int) map[int]int {
-	hh := map[int]int{}
-	for k, v := range h {
-		if v == 1 {
-			hh[k] = 0
-		} else if v == 0 {
-			hh[k] = 1
-		}
-	}
-	return hh
-}
-
-func pprint(m posMap) {
-	min, max := pos{}, pos{}
-	first := true
-	for p, _ := range m {
-		if first {
-			min = p
-			max = p
-			first = false
-		} else {
-			if p.x < min.x {
-				min.x = p.x
-			}
-			if p.y < min.y {
-				min.y = p.y
-			}
-			if p.x > max.x {
-				max.x = p.x
-			}
-			if p.y > max.y {
-				max.y = p.y
-			}
-		}
-	}
-	for j := min.y; j <= max.y; j++ {
-		for i := min.x; i <= max.x; i++ {
-			p := pos{i, j}
-			v, ok := m[p]
-			if !ok {
-				v = 0
-			}
-			if v == 1 {
-				fmt.Printf("#")
-			} else if v == 0 {
-				fmt.Printf(".")
-			}
-		}
-		fmt.Printf("\n")
-	}
-	fmt.Println()
 }
 
 func main() {
@@ -156,16 +80,12 @@ func main() {
 	}
 	zero, _ := h[0]
 	hasInvert := zero == 1
-	if hasInvert {
-		for i := 0; i < 2; i++ {
-			m = step(m, h, i%2 != 0)
-			m = invertMap(m)
-			h = invertH(h)
+	for i := 0; i < 2; i++ {
+		d := 0
+		if hasInvert && i%2 != 0 {
+			d = 1
 		}
-	} else {
-		for i := 0; i < 2; i++ {
-			m = step(m, h, false)
-		}
+		m = step(m, h, d)
 	}
 	r1 := 0
 	for _, v := range m {
@@ -174,16 +94,12 @@ func main() {
 		}
 	}
 	fmt.Println("Part 1:", r1)
-	if hasInvert {
-		for i := 0; i < 48; i++ {
-			m = step(m, h, i%2 != 0)
-			m = invertMap(m)
-			h = invertH(h)
+	for i := 0; i < 48; i++ {
+		d := 0
+		if hasInvert && i%2 != 0 {
+			d = 1
 		}
-	} else {
-		for i := 0; i < 48; i++ {
-			m = step(m, h, false)
-		}
+		m = step(m, h, d)
 	}
 	r2 := 0
 	for _, v := range m {
