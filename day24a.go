071ddcea4fc5219977270f77ca19883b16b1eed0
Alexey Vasilyev <alexey@alexadotlife.com>
2021-12-24 20:24:27 +0100

    day24

---
+++ day24a.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	i := 0
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), " ")
+		if parts[0] == "inp" {
+			fmt.Printf("%s = inp[%d]\n", parts[1], i)
+			i++
+		} else if parts[0] == "add" {
+			fmt.Printf("%s += %s\n", parts[1], parts[2])
+		} else if parts[0] == "mul" {
+			fmt.Printf("%s *= %s\n", parts[1], parts[2])
+		} else if parts[0] == "div" {
+			fmt.Printf("%s = %s // %s\n", parts[1], parts[1], parts[2])
+		} else if parts[0] == "mod" {
+			fmt.Printf("%s = %s %% %s\n", parts[1], parts[1], parts[2])
+		} else if parts[0] == "eql" {
+			fmt.Printf("%s = 1 if %s == %s else 0\n", parts[1], parts[1], parts[2])
+		} else {
+			fmt.Println("ERROR", parts[0])
+		}
+	}
+}
---
+++ day24b.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+var xa []int = []int{ 11, 12, 13, -5, -3, 14, 15, -16, 14, 15, -7, -11, -6, -11 }
+var xb []int = []int{ 16, 11, 12, 12, 12,  2, 11,   4, 12,  9, 10,  11,  6,  15 }
+
+func myFunc(inp []int) (z int) {
+	// decompiled op-code program from the output of day24a
+	z = 0
+	for i := 0; i < 14; i++ {
+		w := inp[i]
+		z = myFuncStep(i, w, z)
+	}
+	return
+}
+
+func myFuncStep(i, w, z int) int {
+	a := xa[i]
+	b := xb[i]
+	if a > 0 {
+		return z * 26 + w + b
+	}
+	zd := z % 26
+	z = z / 26
+	if zd != w - a {
+		return z * 26 + w + b
+	}
+	return z
+}
+
+func main() {
+	zPrev := map[int]int{ 0: 1 }
+	steps := [][]int{
+		[]int{ 0 },
+	}
+	zLimit := 26 * 26 * 26 * 26
+	for i := 0; i < 14; i++ {
+		// for each iteration we build all possible outcomes of myFuncStep
+		// while limiting possible z values, idk, it works
+		zNext := map[int]int{}
+		for z := range zPrev {
+			if z > zLimit {
+				continue
+			}
+			for w := 1; w <= 9; w++ {
+				zz := myFuncStep(i, w, z)
+				zNext[zz] = 1
+			}
+		}
+		zNextArr := []int{}
+		for z := range zNext {
+			zNextArr = append(zNextArr, z)
+		}
+		steps = append(steps, zNextArr)
+		zPrev = zNext
+	}
+	found := false
+	for _, z := range steps[14] {
+		if z == 0 {
+			// 14th iteration outcomes should contain zero
+			// this is the MONAD-valid condition
+			found = true
+			break
+		}
+	}
+	if !found {
+		// if we haven't found it, then increase more zLimit :)
+		fmt.Println("z=0 not found, need more zLimit!")
+		return
+	}
+	steps[14] = []int{ 0 }
+	type pair struct {
+		x, y int
+	}
+	revSteps := [][]int{}
+	revPrev := map[pair]int{ pair{ 0, 0, }: 0, }
+	for i := 14; i >= 1; i-- {
+		// now we rebuild which (w, z)-pairs have led to the valid
+		// condition, so we use cached z-values from the previous step
+		// and go in the oposite direction
+		revNext := map[pair]int{}
+		for _, z := range steps[i-1] {
+			for w := 1; w <= 9; w++ {
+				zz := myFuncStep(i-1, w, z)
+				for p := range revPrev {
+					if p.x == zz {
+						pp := pair{ z, w }
+						revNext[pp] = 1
+						break
+					}
+				}
+			}
+		}
+		// one each step we need only w-values
+		// so we can significantly decrease bruteforce
+		revStep := []int{}
+		hash := map[int]int{}
+		for p := range revNext {
+			_, ok := hash[p.y]
+			if !ok {
+				hash[p.y] = 1
+				revStep = append(revStep, p.y)
+			}
+		}
+		revSteps = append(revSteps, revStep)
+		revPrev = revNext
+	}
+	// each step is the position in the number
+	// for each step we have a list of valid numbers
+	// now we do the bruteforce :)
+	for _, s := range revSteps {
+		// since we only need the largest number
+		// we sort each list from bigger to lower
+		// so the very first match would be the biggest
+		// possible MONAD-value
+		sort.Sort(sort.Reverse(sort.IntSlice(s)))
+	}
+	res := make([]int, 14)
+	found = false
+	var req func(pos int)
+	fmt.Printf("Part 1: ")
+	req = func(pos int) {
+		if found {
+			return
+		}
+		if pos == 14 {
+			z := myFunc(res)
+			if z == 0 {
+				// we only need the very first number
+				for _, w := range res {
+					fmt.Printf("%d", w)
+				}
+				found = true
+			}
+			return
+		}
+		for _, w := range revSteps[pos] {
+			if found {
+				break
+			}
+			res[13-pos] = w
+			req(pos+1)
+		}
+	}
+	req(0)
+	fmt.Println()
+	for _, s := range revSteps {
+		// and for the second part we need the lowest number
+		// so just do the sort from lowest to biggest
+		// and take the first occurance
+		sort.Sort(sort.IntSlice(s))
+	}
+	found = false
+	fmt.Printf("Part 2: ")
+	req(0)
+	fmt.Println()
+}
